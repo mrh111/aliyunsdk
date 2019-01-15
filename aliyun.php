c0210f1ff00097675991bfc6874f5e4aac1c766e
mrh <mrh111@126.com>
2019-01-15 14:44:03 +0800

    整合入口

---
+++ aliyun.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mrh
+ * Date: 19-1-15
+ * Time: 下午2:31
+ */
+namespace aliyunsdk;
+require_once './src/aliyun-php-sdk-core/Config.php';
+
+class aliyun {
+
+}
