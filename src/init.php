13878c43f644a05a7d933da9baa906cac5888f75
mrh <mrh111@126.com>
2019-01-15 14:58:05 +0800

    修改引入文件

---
+++ src/init.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mrh
+ * Date: 19-1-15
+ * Time: 下午2:31
+ */
+namespace AliyunSdk;
+require_once './aliyun-php-sdk-core/Config.php';
+require_once './aliyun-oss-php-sdk/autoload.php';
+
+class init {
+
+}
